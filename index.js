5ad3e45bb9e0c666a0b47aaafd2a103f540bf195
Chance Hudson <jchancehud@gmail.com>
2018-07-18 17:34:19 -0500

    Add support for filename query param

--- index.js
+++ index.js
@@ -4,17 +4,19 @@
 
 const http = require('http');
 const https = require('https');
+const url = require('url');
 
 http.createServer((req, _res) => {
-  const url = req.url;
-  if (url === '/healthcheck') {
+  const path = req.url;
+  const queryParams = url.parse(req.url, true).query;
+  if (path === '/healthcheck') {
     _res.writeHead(204);
     _res.end();
     return;
   }
   https.get({
     hostname: 'circleci.com',
-    path: `/api/v1.1${url}`,
+    path: `/api/v1.1${path}`,
     headers: {
       'Accept': 'application/json'
     }
@@ -31,6 +33,15 @@ http.createServer((req, _res) => {
         const resString = data.join('');
         if (!resString.length) throw new Error('0 length string received');
         const result = JSON.parse(resString);
+        for (var item of result) {
+          if (!queryParams.filename) break;
+          if (item.path.indexOf(queryParams.filename) === -1) continue;
+          _res.writeHead(301, {
+            'Location': item.url
+          });
+          _res.end();
+          return;
+        }
         _res.writeHead(301, {
           'Location': result[0].url
         });
